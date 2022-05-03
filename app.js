3557333e487cff95e4fa219c8399981b8ced97aa
Steffan Crowley <steffancrow@gmail.com>
2022-05-03 10:16:05 -0700

    Add random generator for computer opponent

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
 console.log("rock Paper Scissors");
+
+//returns random number that is assigned to rock, paper, or scissor.
+function computerPlay() {
+  let x = Math.floor(Math.random() * 3);
+  console.log(x);
+  if (x == 0) {
+    let z = "rock";
+    return z;
+  } else if (x == 1) {
+    let z = "paper";
+    return z;
+  } else if (x == 2) {
+    let z = "scissor";
+    return z;
+  }
+}
+
+console.log(computerPlay());
